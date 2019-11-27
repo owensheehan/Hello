7a9837a15e1d3b320316049571ed8d160552baa4
Owen Sheehan <owensheehan@gmail.com>
2019-11-27 21:09:11 +0000

    seventh commit

--- HelloPython.py
+++ HelloPython.py
@@ -3,12 +3,5 @@ print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
-
-
-print ("5")
-
-print("Test 3")
-
-
-#testing auto pipeline jenkins
+#commit 7
 	
