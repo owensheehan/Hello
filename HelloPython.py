3b071a5ad6c44c05347f4cb30bac09d6fafc287e
Owen Sheehan <owensheehan@gmail.com>
2019-11-27 21:02:16 +0000

    sixth commit

--- HelloPython.py
+++ HelloPython.py
@@ -3,12 +3,12 @@ print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
-print ("Test 2")
-<<<<<<< HEAD
+
+
 print ("5")
-=======
+
 print("Test 3")
->>>>>>> 98192f68c8c4cd019be06184ccf59d253c4ab3fb
 
 
+#testing auto pipeline jenkins
 	
