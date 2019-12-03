df300d0ee1636ea741fc5a9d04496ddc0e6d552a
Owen Sheehan <owensheehan@gmail.com>
2019-12-03 18:57:40 +0000

    Version 2.1

--- HelloPython.py
+++ HelloPython.py
@@ -4,11 +4,11 @@ def main():
 	helloWorld()
 	dateTime()
 
-def helloWorld():
+def helloWorld():#Text
 	print("Hello world!")
     print("Version 2")
 	
-def dateTime():
+def dateTime():#date and time
 	now = datetime.datetime.now()
 	print ("Current date and time is ")
 	print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
