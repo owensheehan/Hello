d2261b93d8ae4fabc600f208142a2f12a6d42db4
Owen Sheehan <owensheehan@gmail.com>
2019-12-04 20:28:00 +0000

    fixed indentation

--- HelloPython.py
+++ HelloPython.py
@@ -4,14 +4,14 @@ def main():
 	helloWorld()
 	dateTime()
 
-def helloWorld():#Text
-	print("Hello world!")
+def helloWorld():
+    print("Hello world!")
     print("Version 2")
 	
-def dateTime():#date and time
-	now = datetime.datetime.now()
-	print ("Current date and time is ")
-	print (now.strftime("%A, %d-%m-%Y : %H:%M"))
+def dateTime():
+    now = datetime.datetime.now()
+    print ("Current date and time is ")
+    print (now.strftime("%A, %d-%m-%Y : %H:%M"))
     print("Goodbye")	
 	#azure2
 main()
