bfffab2549691f87c8a92b577cb2480923ab6856
Owen Sheehan <owen.sheehan@mycit.ie>
2019-12-08 22:26:25 +0000

    Update HelloPython.py

--- HelloPython.py
+++ HelloPython.py
@@ -3,8 +3,7 @@ import datetime
 def main():
 	helloWorld()
 	dateTime()
-	hold()
-	
+
 def helloWorld():
     print("Hello world!")
     print("Version 2")
@@ -15,10 +14,6 @@ def dateTime():
     print (now.strftime("%A, %d-%m-%Y : %H:%M"))
     print("Goodbye")	
 	#azure2
-	
-def hold():
-	a = raw_input("Press Enter to continue...")
-	print(a)
 	
 def multiplication(x, y):
     return x*y
